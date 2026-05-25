f9e99d57705c5e0f151b34ac32c6e26b77408182
kulane952 <kulaneshurwac@gmail.com>
2026-05-25 14:38:57 +0300

    Exercide 10 javaScript

---
+++ Exercise10/app.js
@@ -0,0 +1,20 @@
+let library=[
+    {
+        title:"The Great Gatsby",
+        author:"F. Scott Fitzgerald",
+        year:1925
+    },
+    {
+        title:"To Kill a Mockingbird",
+        author:"Harper Lee",
+        year:1960
+    },
+    {
+        title:"1984",
+        author:"George Orwell",
+        year:1948
+    }
+]
+
+console.log(library[0].title);
+console.log(library[1].author);
