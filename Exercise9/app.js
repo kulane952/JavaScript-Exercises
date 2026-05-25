561ac4ec63e02dbf8d261ff867ce5eb741596563
kulane952 <kulaneshurwac@gmail.com>
2026-05-25 14:08:47 +0300

    JavaScript Exersice

---
+++ Exercise9/app.js
@@ -0,0 +1,17 @@
+let card={
+    make:"Toyota",
+    model:"Camry",
+    year:"2020"
+}
+
+console.log(card.make);
+console.log(card.model);
+console.log(card.year);
+
+console.log("after adding new property");
+
+card.start='2020-01-01';
+
+console.log("the card has start: " + card.start);
+
+// console.log(card);
