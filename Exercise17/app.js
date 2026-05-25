ffc6c1225c3247b7154becb96521d534eb883e66
kulane952 <kulaneshurwac@gmail.com>
2026-05-25 20:24:17 +0300

    Exercide 17 javaScript

---
+++ Exercise17/app.js
@@ -0,0 +1,11 @@
+let temperature=24;
+if(temperature<0){
+    console.log("Verry Cold")
+}else if(temperature >=0 && temperature<15){
+
+    console.log("Cold")
+}else if(temperature >=15 && temperature<25){
+    console.log("Warm")
+}else{
+    console.log("Hot")
+}
