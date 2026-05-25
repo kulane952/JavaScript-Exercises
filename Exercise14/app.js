207395e3293f140911c3f8155031bcdd76295ba6
kulane952 <kulaneshurwac@gmail.com>
2026-05-25 18:48:41 +0300

    Exercide 14 javaScript

---
+++ Exercise14/app.js
@@ -0,0 +1,17 @@
+// let number;
+
+// do{
+//     number=prompt("Please enter a number that is greater than 10");
+
+// }while(number <= 10);
+
+// console.log("thank you for your number");
+
+
+let number;
+
+do {
+    number = prompt("Please enter a number that is greater than 10");
+} while (number <= 10);
+
+console.log("Thank you for your number");
